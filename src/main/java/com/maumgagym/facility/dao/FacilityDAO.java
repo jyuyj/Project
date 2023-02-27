f08c0b2f3e4f9f8cb9b7bce34d993b660e146c6f
jyuyj <hyunj9212@gmail.com>
2023-02-27 16:28:24 +0900

    [update]facilityDAO 수정

--- src/main/java/com/maumgagym/facility/dao/FacilityDAO.java
+++ src/main/java/com/maumgagym/facility/dao/FacilityDAO.java
@@ -25,22 +25,7 @@ public class FacilityDAO {
 	@Autowired
 	private DataSource dataSource;
 	
-//	private String uploadPath = "C:/eGovFrameDev-4.0.0-64bit/project/SpringBoot_Maumgagym/src/main/webapp/upload";
-
-	//DB연결
-	public FacilityDAO() {
-		// TODO Auto-generated constructor stub
-		
-		try {
-			Context init = new InitialContext();
-			Context env = (Context)init.lookup( "java:comp/env" );
-			this.dataSource = (DataSource)env.lookup( "jdbc/mariadb1" );
-		} catch (NamingException e) {
-			// TODO Auto-generated catch block
-			System.out.println( "[에러] " + e.getMessage() );
-		}
-
-	}	
+	private String uploadPath = "C:/eGovFrameDev-4.0.0-64bit/project/Project/src/main/webapp/upload";
 	
 	public ArrayList<FacilityTO> facilityList() {
 		
@@ -53,8 +38,10 @@ public class FacilityDAO {
 		try {
 			conn = this.dataSource.getConnection();
 			
+			// 글 게시판의 title을 통한 업체 정보, 멤버 게시판의 address을 통한 주소 정보, 멤버쉽 게시판의 price를 통한 가격 정보, 이미지 게시판의 name을 통한 이미지 파일, 태그 게시판의 tag를 통한 글 태그를 가져옴
+			// 카테고리 게시판의 seq는 1~9번(운동시설)으로 가져옴
 			String sql = "SELECT b.seq, b.title, b.category_seq, m.address, ms.price, i.name, t.tag "
-					+ "FROM board b LEFT OUTER JOIN member m ON ( b.write_seq = m.seq ) "
+					+ "FROM board b LEFT OUTER JOIN member m ON( b.write_seq = m.seq ) "
 					+ "LEFT OUTER JOIN membership ms ON( b.seq = ms.board_seq ) "
 					+ "LEFT OUTER JOIN image i ON( b.seq = i.board_seq ) "
 					+ "LEFT OUTER JOIN tag t ON( b.seq = t.board_seq ) "
@@ -91,82 +78,6 @@ public class FacilityDAO {
 		return facilityLists; 
 		
 	}
-//	public ArrayList facility() {
-//		//System.out.println( "FacilityDAO()호출" );
-//		
-//		Connection conn = null;
-//		PreparedStatement pstmt = null;
-//		ResultSet rs = null;
-//		
-//		Map<String, Object> map = new HashMap<>();
-//
-//		ArrayList datas = new ArrayList<>();
-//		
-//		try {
-//			conn = this.dataSource.getConnection();
-//			
-//			// 글 게시판의 title을 통한 업체 정보, 멤버 게시판의 address을 통한 주소 정보, 멤버쉽 게시판의 price를 통한 가격 정보, 이미지 게시판의 name을 통한 이미지 파일, 태그 게시판의 tag를 통한 글 태그를 가져옴
-//			// 카테고리 게시판의 seq는 1~9번(운동시설)으로 가져옴
-//			StringBuilder sbDatas = new StringBuilder();   			   
-//			sbDatas.append( " SELECT b.seq, b.title, b.category_seq, m.address, ms.price, i.name, t.tag" );
-//			sbDatas.append( "    FROM board b LEFT OUTER JOIN member m" );
-//			sbDatas.append( "          ON ( b.write_seq = m.seq ) LEFT OUTER JOIN membership ms" );
-//			sbDatas.append( "             ON( b.seq = ms.board_seq ) LEFT OUTER JOIN image i" );
-//			sbDatas.append( "                ON( b.seq = i.board_seq ) LEFT OUTER JOIN tag t" );
-//			sbDatas.append( "                   ON( b.seq = t.board_seq ) LEFT OUTER JOIN category c" );
-//			sbDatas.append( "                      ON( b.category_seq = c.seq )" );
-//			sbDatas.append( "							WHERE c.seq <= 9 && b.status=1" );
-//			sbDatas.append( "								group BY b.seq;" );
-//			//sbDatas.append( "select t.tag from tag t left outer join board b on( t.board_seq = b.seq );" );
-//			   
-//			   
-//			// 변수에 대입
-//			String sql = sbDatas.toString(); 
-//			pstmt = conn.prepareStatement(sql);
-//			
-//			rs = pstmt.executeQuery();
-//			
-//			int i = 0;
-//			while( rs.next() ) {
-//				 i++;
-//				
-//				 
-//				 
-//				BoardTO bto = new BoardTO();
-//				bto.setSeq(rs.getInt( "b.seq" ));
-//				bto.setTitle(rs.getString( "b.title" ));
-//				bto.setTag(rs.getString( "t.tag" ));
-//				bto.setCategory_seq(rs.getInt( "b.category_seq" ));
-//				bto.setImage_name( rs.getString( "i.name" ) );
-//			
-//				MemberTO mto = new MemberTO();
-//				mto.setAddress(rs.getString("m.address"));
-//				
-//				MemberShipTO msto = new MemberShipTO();
-//				msto.setMembership_price( rs.getInt( "ms.price" ) );
-//				
-////				System.out.println( bto.getTitle()); 
-////				System.out.println ( "dao : " + mto.getAddress());
-////				System.out.println( msto.getMembership_price());
-//				
-//				map.put("bto" + i, bto);
-//				map.put("mto" + i, mto);
-//				map.put("msto" + i, msto);
-//				datas.add(map);
-//				}
-//				//System.out.println( datas.size() );
-//
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			System.out.println( "[에러] " + e.getMessage() );
-//		} finally {
-//			if(rs != null) try {rs.close();} catch(SQLException e) {}
-//			if(pstmt != null) try {pstmt.close();} catch(SQLException e) {}
-//			if(conn != null) try {conn.close();} catch(SQLException e) {}
-//		}
-//		    
-//		return datas;
-//	}
 	
 	public BoardTO selectfacilityBoard( BoardTO to ) {
 		
